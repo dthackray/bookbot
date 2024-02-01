5f6cfdb6ebd55cf280a4d980f2a492f69453912a
dan <djthackray@gmail.com>
2024-02-01 12:14:36 +0000

    Added character counting function

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     print(text)
+    print("\n--- Begin report of books/frankenstein.txt ---\n")
     words = count_words(text)
-    print(f"{words} words found in the text.")
+    print(f"{words} words found in the text.\n")
     characters = count_characters(text)
 
 def get_book_text(path):
@@ -17,6 +18,11 @@ def count_words(text):
 
 def count_characters(text):
     characters_dict = {e:text.count(e) for e in sorted(set(text.lower()))}
-    return characters_dict
+    characters_dict = sorted(characters_dict.items(), key=lambda x: x[1], reverse=True)
+
+    characters_list = list(characters_dict)
+    for character in characters_list:
+        if character[0].isalpha():
+            print(f"The '{character[0]}' character was found {character[1]} times.")
 
 main()
