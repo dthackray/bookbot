c53ab16727f76a09e6cba5383e708644aee402e0
dan <djthackray@gmail.com>
2024-02-01 11:53:42 +0000

    Small refactor to the output of the count_words function

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ def main():
     text = get_book_text(book_path)
     print(text)
     words = count_words(text)
-    print(words)
+    print(f"{words} found in the text.")
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 def count_words(text):
-    words = len(text.split())
-    return words
+    words = text.split()
+    return len(words)
 
 main()
