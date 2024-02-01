92dac0781dd92b658067b9a33185383a6291e6a9
dan <djthackray@gmail.com>
2024-02-01 12:03:06 +0000

    Added count_character function - returns dictionary

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ def main():
     text = get_book_text(book_path)
     print(text)
     words = count_words(text)
-    print(f"{words} found in the text.")
+    print(f"{words} words found in the text.")
+    characters = count_characters(text)
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,6 +12,11 @@ def get_book_text(path):
 
 def count_words(text):
     words = text.split()
-    return len(words)
+    number_of_words = len(words)
+    return number_of_words
+
+def count_characters(text):
+    characters_dict = {e:text.count(e) for e in sorted(set(text.lower()))}
+    return characters_dict
 
 main()
